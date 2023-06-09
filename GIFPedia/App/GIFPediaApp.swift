78c4406f75a356ba2a9833c073a9760143c06b81
Yabby1997 <yabby1997@gmail.com>
2023-06-09 19:06:37 +0900

    Initial Commit

---
+++ GIFPedia/App/GIFPediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIFPediaApp.swift
+//  GIFPedia
+//
+//  Created by USER on 2023/06/09.
+//
+
+import SwiftUI
+
+@main
+struct GIFPediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
